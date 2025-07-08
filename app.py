1adb0472652e0e4ed48eaf028b448ec6d94d349b
Janvi <janvi.janvi1821@gmail.com>
2025-07-08 17:38:48 +0530

    Added!

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'techweldengineers@gmail.com'       # Your email
-app.config['MAIL_PASSWORD'] = 'hsxy lacg trjy azjm'          # App password (not your Gmail password)
+app.config['MAIL_PASSWORD'] = 'ooqqakyknqwkdmtu'          # App password (not your Gmail password)
 app.config['MAIL_DEFAULT_SENDER'] = 'techweldengineers@gmail.com'
 
 mail = Mail(app)
